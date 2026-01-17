2462b1d7b74db2d460dca0cdaf11e26a38d6a06f
asaini987 <sainiraaditya@gmail.com>
2026-01-16 22:23:11 -0500

    Fixed mobile photo rotation with EXIF orientation

--- src/utils/grayscale_image.py
+++ src/utils/grayscale_image.py
@@ -1,10 +1,14 @@
-from PIL import Image
+from PIL import Image, ImageOps
 import io
 import base64
 import js
 
 def grayscale_image(image_bytes):
     image = Image.open(io.BytesIO(image_bytes))
+    
+    # apply EXIF orientation for mobile photos
+    image = ImageOps.exif_transpose(image)
+    
     grayscaled_image = image.convert("L")
     
     original_format = image.format or "PNG"
